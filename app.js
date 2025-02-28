90bfeae08dc9da51e1bfce913889422bc350e822
= <ricksarkar2005@gmail.com>
2025-02-28 21:08:24 +0530

    Modified for both commonjs and module

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use("/api", api );
 
 app.use(errorHandlerMiddleware);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-});
+// app.listen(process.env.PORT, () => {
+//     console.log(`Server running on port ${process.env.PORT}`);
+// });
+
+export default app;
