5801685e53b73bcc60bb83b3afdf63f6dd187429
= <ricksarkar2005@gmail.com>
2025-03-15 17:03:57 +0530

    JWT Expiry duration changed to 7 Days.

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 export const generateToken = (payload) => {
     return jwt.sign( payload , process.env.JWT_SECRET , {
-        expiresIn: "3d",
+        expiresIn: "7d",
         issuer: "tech-frenzy"
     });
 };
